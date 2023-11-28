b4d9a05a7df98a60989db62b897378772ca85096
Arthur Junior <arthurjr1906@gmail.com>
2023-11-28 20:00:40 -0300

    feat: Aside implement

---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,23 @@
+import { HomeIcon, Search } from 'lucide-react';
+import Link from 'next/link';
+import PlaylistSection from './PlaylistSection';
+
+export default function Aside() {
+  return (
+    <aside className="p-2 w-[437px] text-main-color">
+      <section className="flex flex-col gap-6 bg-second-color py-5 pl-5 rounded-lg w-full h-28">
+        <Link href="#" className="flex w-full gap-4 text-selected-color font-semibold">
+          <HomeIcon />
+          Home
+        </Link>
+        
+        <Link href="#" className="flex w-full gap-4 hover:text-selected-color font-semibold">
+          <Search />
+          Search
+        </Link>
+      </section>
+      
+      <PlaylistSection />
+    </aside>
+  )
+};
