aeab97e3f70aaef32dd160b3690ce667114d4053
Arthur Junior <arthurjr1906@gmail.com>
2023-11-29 23:27:02 -0300

    fix: Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Aside from '@/components/Aside/Aside';
 import Main from '@/components/Main-Content/Main';
-import { Bell, ChevronLeft, ChevronRight, Play, User } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
-import albumCover from '../../public/album-cover.png';
 
 export default function Home() {
   return (
