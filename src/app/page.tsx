97dd64561ae454d414251dcb968625034491bf51
Arthur Junior <arthurjr1906@gmail.com>
2023-11-29 23:26:33 -0300

    feat: add main content on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Aside from '@/components/Aside/Aside';
+import Main from '@/components/Main-Content/Main';
+import { Bell, ChevronLeft, ChevronRight, Play, User } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import albumCover from '../../public/album-cover.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <Aside />
+    <main className="flex flex-col">
+      <div className="flex h-[584px]">
+        <Aside />
+
+        <Main />
+      </div>
     </main>
   )
 }
