30b2717c0623e7a1719cb821c646e825ecd13449
Arthur Junior <arthurjr1906@gmail.com>
2023-12-01 01:20:07 -0300

    feat: Add Player component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Aside from '@/components/Aside/Aside';
 import Main from '@/components/Main-Content/Main';
+import Player from '@/components/Player/Player';
 
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <div className="flex h-[584px]">
+      <div className="flex xl:h-full flex-grow min-h-[610px] max-h-[88vh]">
         <Aside />
-
         <Main />
       </div>
+
+      <Player />
     </main>
   )
 }
