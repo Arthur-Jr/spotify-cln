287df5c4a6ea02c97b07934b57f77bf4844cc2a9
Arthur Junior <arthurjr1906@gmail.com>
2023-12-03 15:23:21 -0300

    feat: add btn to original route

--- src/components/Main-Content/Header.tsx
+++ src/components/Main-Content/Header.tsx
@@ -1,5 +1,5 @@
 import { Bell, ChevronLeft, ChevronRight, User } from 'lucide-react';
-import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
@@ -14,7 +14,15 @@ export default function Header() {
         </button>
       </div>
 
-      <div className="flex gap-2 text-selected-color">
+      <div className="flex gap-2 text-selected-color items-center">
+        <Link
+          href="/original"
+          target="_blank"
+          className="py-2 px-4 text-sm font-semibold rounded-full bg-zinc-950/70 hover:scale-105 hover:bg-zinc-950/90"
+        >
+          Original version print
+        </ Link>
+
         <button type="button" className="p-2 rounded-full bg-zinc-950/70 hover:scale-110 hover:bg-zinc-950/90" title="What's New">
           <Bell width={18} height={18} />
         </button>
